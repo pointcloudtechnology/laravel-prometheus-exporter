9f82cde7fe66a9dfa94d50b0714ece71e9475a41
Adrian Holfter <adrian.holfter@pointcloudtechnology.com>
2024-04-22 15:47:53 +0200

    skip APCu and Redis if extension is not enabled

--- tests/StorageAdapterFactoryTest.php
+++ tests/StorageAdapterFactoryTest.php
@@ -19,6 +19,10 @@ class StorageAdapterFactoryTest extends TestCase
 
     public function testMakeApcAdapter()
     {
+        if (!extension_loaded('apcu') || !apcu_enabled()) {
+            $this->markTestSkipped('APCu extension is not loaded or not enabled.');
+        }
+
         $factory = new StorageAdapterFactory();
         $adapter = $factory->make('apc');
         $this->assertInstanceOf(APC::class, $adapter);
@@ -26,6 +30,10 @@ class StorageAdapterFactoryTest extends TestCase
 
     public function testMakeRedisAdapter()
     {
+        if (!extension_loaded('redis')) {
+            $this->markTestSkipped('Redis extension is not loaded.');
+        }
+
         $factory = new StorageAdapterFactory();
         $adapter = $factory->make('redis');
         $this->assertInstanceOf(Redis::class, $adapter);
